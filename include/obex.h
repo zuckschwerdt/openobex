abf99e0fdc62027411b98f6b07032fa3d7cc004a
Marcel Holtmann <marcel@holtmann.org>
2002-10-28 21:58:54 +0000

    Fix workaround for Bluetooth support, again

--- include/obex.h
+++ include/obex.h
@@ -49,7 +49,7 @@ typedef void* obex_t;
 typedef void* obex_object_t;
 typedef void (*obex_event_t)(obex_t *handle, obex_object_t *obj, int mode, int event, int obex_cmd, int obex_rsp);
 // This is to workaround compilation without Bluetooth support. - Jean II
-#ifndef BDADDR_ANY
+#ifndef AF_BLUETOOTH
 typedef char* bdaddr_t;
 #endif
 
