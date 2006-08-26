115866b909f07332dfd145962bd24e69414346b1
Marcel Holtmann <marcel@holtmann.org>
2006-08-26 17:53:58 +0000

    Add example test client

---
+++ glib/test-client.c
@@ -0,0 +1,39 @@
+/*
+ *
+ *  OpenOBEX - Free implementation of the Object Exchange protocol
+ *
+ *  Copyright (C) 2005-2006  Marcel Holtmann <marcel@holtmann.org>
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "obex-client.h"
+
+int main(int argc, char *argv[])
+{
+	GObject *client;
+
+	client = obex_client_new();
+
+	g_object_unref(client);
+
+	return 0;
+}
