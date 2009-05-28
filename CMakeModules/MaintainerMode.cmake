cb2b4100960bbdeb4414cb9e43ed24cbacf3e5af
Hendrik Sattler <post@hendrik-sattler.de>
2009-05-28 20:44:13 +0200

    CMake: flags are compiler specific, so only set them if gcc is used

--- CMakeModules/MaintainerMode.cmake
+++ CMakeModules/MaintainerMode.cmake
@@ -1,20 +1,22 @@
 option ( USE_MAINTAINER_MODE "Enable some stuff only relevant to developers" OFF )
 if ( USE_MAINTAINER_MODE )
-  set ( MAINTAINER_MODE_WARN_FLAGS
-    extra
-    no-unused-parameter
-    no-missing-field-initializers
-    declaration-after-statement
-    missing-declarations
-    redundant-decls
-    cast-align
-  )
-  foreach ( flag ${MAINTAINER_MODE_WARN_FLAGS} )
-    set ( cflag "-W${flag}" )
-    set ( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${cflag}" )
-    set ( CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS} ${cflag}" )
-    set ( CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS} ${cflag}" )
-    set ( CMAKE_C_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS} ${cflag}" )
-    set ( CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS} ${cflag}" )      
-  endforeach ( flag )
+  if ( CMAKE_COMPILER_IS_GNUCC )
+    set ( MAINTAINER_MODE_WARN_FLAGS
+      extra
+      no-unused-parameter
+      no-missing-field-initializers
+      declaration-after-statement
+      missing-declarations
+      redundant-decls
+      cast-align
+    )
+    foreach ( flag ${MAINTAINER_MODE_WARN_FLAGS} )
+      set ( cflag "-W${flag}" )
+      set ( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${cflag}" )
+      set ( CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS} ${cflag}" )
+      set ( CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS} ${cflag}" )
+      set ( CMAKE_C_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS} ${cflag}" )
+      set ( CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS} ${cflag}" )      
+    endforeach ( flag )
+  endif ( CMAKE_COMPILER_IS_GNUCC )
 endif ( USE_MAINTAINER_MODE )
