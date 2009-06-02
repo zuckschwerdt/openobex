e3d8088e498d1f32243a213eb24ecba2de040ace
Hendrik Sattler <post@hendrik-sattler.de>
2009-06-02 22:27:20 +0200

    adapt maintainer mode for MSVC

--- CMakeModules/MaintainerMode.cmake
+++ CMakeModules/MaintainerMode.cmake
@@ -20,5 +20,19 @@ if ( USE_MAINTAINER_MODE )
       set ( CMAKE_C_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS} ${cflag}" )
       set ( CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS} ${cflag}" )      
     endforeach ( flag )
+
+  elseif ( MSVC )
+    set ( MAINTAINER_MODE_WARN_FLAGS
+      W3
+      WX
+    )
+    foreach ( flag ${MAINTAINER_MODE_WARN_FLAGS} )
+      set ( cflag "/${flag}" )
+      set ( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${cflag}" )
+      set ( CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS} ${cflag}" )
+      set ( CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS} ${cflag}" )
+      set ( CMAKE_C_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS} ${cflag}" )
+      set ( CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS} ${cflag}" )      
+    endforeach ( flag )
   endif ( CMAKE_COMPILER_IS_GNUCC )
 endif ( USE_MAINTAINER_MODE )
