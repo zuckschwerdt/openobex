95ef4382ad9240110774e3bfabbd3724d02cfd00
Hendrik Sattler <post@hendrik-sattler.de>
2009-07-04 18:20:11 +0200

    extend maintainer mode

--- CMakeModules/MaintainerMode.cmake
+++ CMakeModules/MaintainerMode.cmake
@@ -12,27 +12,37 @@ if ( USE_MAINTAINER_MODE )
       cast-align
       error
     )
+    set ( MAINTAINER_MODE_FLAGS
+#      pedantic
+#      std=c99
+    )
     foreach ( flag ${MAINTAINER_MODE_WARN_FLAGS} )
-      set ( cflag "-W${flag}" )
+      list ( APPEND MAINTAINER_MODE_FLAGS "W${flag}" )
+    endforeach ( flag )
+
+    foreach ( flag ${MAINTAINER_MODE_FLAGS} )
+      set ( cflag "-${flag}" )
       set ( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${cflag}" )
-      set ( CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS} ${cflag}" )
-      set ( CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS} ${cflag}" )
-      set ( CMAKE_C_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS} ${cflag}" )
-      set ( CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS} ${cflag}" )      
+      foreach ( type DEBUG RELEASE MINSIZEREL RELWITHDEBINFO )
+	set ( CMAKE_C_FLAGS_${type} "${CMAKE_C_FLAGS_${type}} ${cflag}" )
+      endforeach ( type )
     endforeach ( flag )
 
   elseif ( MSVC )
-    set ( MAINTAINER_MODE_WARN_FLAGS
+    set ( MAINTAINER_MODE_FLAGS
       W3
       WX
     )
-    foreach ( flag ${MAINTAINER_MODE_WARN_FLAGS} )
+    foreach ( flag ${MAINTAINER_MODE_FLAGS} )
       set ( cflag "/${flag}" )
       set ( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${cflag}" )
-      set ( CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS} ${cflag}" )
-      set ( CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS} ${cflag}" )
-      set ( CMAKE_C_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS} ${cflag}" )
-      set ( CMAKE_C_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS} ${cflag}" )      
+      foreach ( type DEBUG RELEASE MINSIZEREL RELWITHDEBINFO )
+	set ( CMAKE_C_FLAGS_${type} "${CMAKE_C_FLAGS_${type}} ${cflag}" )
+      endforeach ( type )
     endforeach ( flag )
   endif ( CMAKE_COMPILER_IS_GNUCC )
+
+#  foreach ( type DEBUG RELEASE MINSIZEREL RELWITHDEBINFO )
+#    message ( "CMAKE_C_FLAGS_${type} set to \"${CMAKE_C_FLAGS_${type}}\"" )
+#  endforeach ( type )
 endif ( USE_MAINTAINER_MODE )
